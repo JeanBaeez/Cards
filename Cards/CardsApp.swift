15fcc49c602f105f9f62bcab53c81243694e2e3b
JeanBaeez <jlbaez44@gmail.com>
2021-09-04 15:53:28 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Jean Luis Baez on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
